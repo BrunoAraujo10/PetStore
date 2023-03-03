c45b6c3a6f55fb11bfd6139f76b07aeed4077605
BrunoAraujo10 <brunoester2016@hotmail.com>
2023-03-03 16:20:48 -0300

    Completei o script consultarPet (Get)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -16,21 +16,37 @@ public class Pet {
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
-    @Test
+    @Test(priority = 1)
     public void incluirPet() throws IOException {
     String jsonBody = lerJson("db/pet1.json");
-
 
      given().contentType("application/json").log().all().body(jsonBody)
     .when().post(uri)
     .then().log().all().statusCode(200)
     .body("name", is("helloBob"))
     .body("status", is("available"))
-    .body("category.name", is("dog"))
+    .body("category.name", is("XPTO2023"))
     .body("tags.name",contains("api"))
 
+     // post
      ;
 
+    }
+    @Test(priority = 2)
+    public void consultarPet(){
+        String petId = "19882023";
+        String token =
+        given().contentType("application/json").log().all()
+        .when().get(uri + "/" + petId).then().log().all().statusCode(200)
+        .body("name", is("helloBob"))
+        .body("category.name", is("XPTO2023"))
+        .body("status", is("available"))
+        .extract() .path("category.name");
+        System.out.println("O token é " + token);
+
+        // get
+
+     ;
     }
 
 

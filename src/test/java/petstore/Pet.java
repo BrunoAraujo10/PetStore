12f9211f962ce8527db41040903dc7e45085a0dc
BrunoAraujo10 <brunoester2016@hotmail.com>
2023-03-03 15:04:33 -0300

    Completei o script incluirPet (post)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -7,6 +7,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
 
 public class Pet {
     String  uri = "https://petstore.swagger.io/v2/pet";
@@ -22,6 +24,10 @@ public class Pet {
      given().contentType("application/json").log().all().body(jsonBody)
     .when().post(uri)
     .then().log().all().statusCode(200)
+    .body("name", is("helloBob"))
+    .body("status", is("available"))
+    .body("category.name", is("dog"))
+    .body("tags.name",contains("api"))
 
      ;
 
